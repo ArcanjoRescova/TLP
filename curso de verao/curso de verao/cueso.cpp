dc0f581b60d1f53af8d30a930063225087e60060
Arcanjo Rescova <155965142+ArcanjoRescova@users.noreply.github.com>
2024-02-27 14:16:51 +0000

    Add files via upload

---
+++ curso de verao/curso de verao/cueso.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>​
+#include <string>​
+#include <locale.h>
+
+int main() {
+	using namespace std;
+	int diasu,grupo, i,total,decimal,valor;
+	float  preço=51.50;
+	do
+	{
+		    preço=preço*0.25;
+			cout << preço;
+			cin >> diasu;
+			cin >> grupo;
+	} while (preço < 100 && preço>1000);
+
+			if (grupo>4)
+			{
+				preço=preço-preço* 0.50;
+			}
+			decimal = ceil(preço) - (preço);
+			if (decimal > 0.50)
+			{
+				valor = ceil(preço) - 0.50;
+			}
+			else
+			{
+				valor = ceil(preço);
+			}
+			cout << valor;
+			if (diasu>4||diasu>8)
+			{
+				 preço = preço - preço * 0.4;
+				 cout << preço;
+			}
+			else if (diasu > 8)
+			{
+				preço = preço - preço * 0.65;
+			}
+
+	
+}
